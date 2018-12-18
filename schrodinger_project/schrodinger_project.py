5189c5eafc514cb061b18fa9c9e0d24cdcbbc74c
Jinyu Xu <jxu52@u.rochester.edu>
2018-12-18 15:03:43 +0800

    code completion

--- schrodinger_project/schrodinger_project.py
+++ schrodinger_project/schrodinger_project.py
@@ -1,3 +1,112 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import tensorflow as tf
+import argparse
+import math
+tf.enable_eager_execution()
+tfe = tf.contrib.eager
+
+def getinput():
+    input = argparse.ArgumentParser()
+    input.add_argument('--data_table',  nargs='*', default=[[0,0], [1.57079,6], [3.14159,0]], help = 'Input of data table' )
+    input.add_argument('--c', type = float, default = 1, help = 'Value of c, default = 1' )
+    input.add_argument('--basis_size', type = int, default = 3, help = 'Size of basis set' )
+    data = input.parse_args()
+    return data
+
+def calculateLHSValue(data, index1, index2):
+
+    # This function calculate the value of scalar.
+    # input: data table, index number1, index number2
+    # output: scalar
+
+    scalar = 0
+    if index1 % 2 == 0:
+        if index2 % 2 == 0:
+            for i in range(len(data)):
+                scalar += math.cos(index2 / 2 * data[i][0])* math.cos(index1 / 2 * data[i][0])
+            return scalar
+
+        else:
+            for i in range(len(data)):
+                scalar += math.sin((index2 + 1) / 2 * data[i][0])* math.cos(index1 / 2 * data[i][0])
+            return scalar
+        
+    else:
+        if index2 % 2 == 0:
+            for i in range(len(data)):
+                scalar += math.cos(index2 / 2 * data[i][0])* math.sin((index1 + 1) / 2 * data[i][0])
+            return scalar
+
+        else:
+            for i in range(len(data)):
+                scalar += math.sin((index2 + 1) / 2 * data[i][0])* math.sin((index1 + 1) / 2 * data[i][0])
+            return scalar    
+
+def calculateV0(data, size):
+
+    # This function calculate the V0 of given data.
+    # input: data table, size of basis
+    # output: V0
+
+    RHS = []
+    LHS = []
+    for j in range(size):
+        if j % 2 != 0:
+            sumR = [0]
+            scalarL = []
+            for k in range(size):
+                sumR[0] += math.sin((j + 1) / 2 *data[k][0])* data[k][1]
+                scalarL.append(calculateLHSValue(data, j, k))
+            RHS.append(sumR)
+            LHS.append(scalarL)
+            
+        else:
+            sumR = [0]
+            scalarL = []
+            for k in range(size):
+                sumR[0] += math.cos(j / 2 *data[k][0])* data[k][1]
+                scalarL.append(calculateLHSValue(data, j, k))
+            RHS.append(sumR)
+            LHS.append(scalarL)
+    RHS_tensor = tf.Variable(RHS, dtype=tf.float32)
+    LHS_tensor = tf.Variable(LHS, dtype=tf.float32)
+    V0 = tf.linalg.solve(LHS_tensor, rhs = RHS_tensor)
+
+    return V0
+
+def buildHHat(V0, size, c):
+
+    # This function build the H hat matix.
+    # input: V0, size of basis, c
+    # output: H hat
+    sess = tf.Session()
+    with sess.as_default():
+        v0=V0.numpy()
+    hHat = [[v0[i][0]]* size for i in range(size)]
+    for i in range(size):
+        hHat[i][i] += i ** 2 * c
+    hHat_tensor = tf.Variable(hHat, dtype=tf.float32)
+    return hHat_tensor
+                
+def main():
+
+    # This function start the program.
+    # input: /
+    # output: /
+
+    data = getinput()
+    v0Hat=calculateV0(data.data_table,data.basis_size)
+    hHat = buildHHat(v0Hat, data.basis_size, data.c)
+    e_tensor, v_tensor = tf.linalg.eigh(hHat)
+    sess = tf.Session()
+    with sess.as_default():
+        e=e_tensor.numpy()
+        v=v_tensor.numpy()
+
+    print(e)
+    print(v)
+
+if __name__ == "__main__":
+    main()
--- tests/test_schrodinger_project.py
+++ tests/test_schrodinger_project.py
@@ -2,13 +2,54 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `schrodinger_project` package."""
-
+import unittest
 import pytest
+import tensorflow as tf
+import math
+from schrodinger_project import schrodinger_project as sp
+
+def checkEqual(a, b):
+    # This function check if two given value are equal
+    # input: a, b
+    # output: T/F
+    difference = abs(a - b)
+    if difference <0.00001:
+        return True
+    return False
+
+class Test(unittest.TestCase):
+    
+    def input_test(self):
+        testinput = sp.getinput()
+        self.assertEqual(testinput.c, 1)
+        self.assertEqual(testinput.basis_size, 3)
+        self.assertEqual(testinput.data_table[0][0],0)
+    
+    def LHSScalar_test(self):
+        testdata1 = [[0, 0]]
+        self.assertEqual(sp.calculateLHSValue(testdata1, 0, 0), 1)
+    
+    def v0_test(self):
+        sess = tf.Session()
+        testdata = sp.getinput()
+        V = sp.calculateV0(testdata.data_table, testdata.size)
+        with sess.as_default():
+            v0 = V.numpy()
+        self.assertTrue(checkEqual(v0[0,0], 6))
+
+    def hhat_test(self):
+        sess = tf.Session()
+        testdata = sp.getinput()
+        V = sp.calculateV0(testdata.data_table, testdata.size)
+        hHat = sp.buildHHat(V, testdata.basis_size, testdata.c)
+        with sess.as_default():
+            h = hHat.numpy()
+        self.assertTrue(checkEqual(h[1][1], 1))
 
 
-from schrodinger_project import schrodinger_project
 
 
+'''
 @pytest.fixture
 def response():
     """Sample pytest fixture.
@@ -23,3 +64,4 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+'''
