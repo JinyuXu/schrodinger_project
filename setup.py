cde993dcb76081cf1396844be2dafa0e1934d8f3
Jinyu Xu <jxu52@u.rochester.edu>
2018-12-19 09:27:11 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [ ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ['pytest-runner', 'tensorflow']
 
 test_requirements = ['pytest', ]
 
